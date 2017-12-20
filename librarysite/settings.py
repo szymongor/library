adcab3a80e9904d742655aebd42ea98346483552
szymongor <gornioczek.szymon@gmail.com>
2017-12-20 16:32:30 +0100

    Allowed hosts

--- librarysite/settings.py
+++ librarysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')eu7q%maa(4h5x&74rcqx-^j-g-r4yax%#1(jj=)7wp=pvgn!d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['szymongor.pythonanywhere.com']
 
 
 # Application definition
