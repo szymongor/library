84f0eeb23d41d5714114226480d1332d9156cdd6
szymongor <gornioczek.szymon@gmail.com>
2017-12-27 16:50:54 +0100

    Token api

--- libraryapp/views.py
+++ libraryapp/views.py
@@ -5,8 +5,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Ksiazka
 from .serializers import KsiazkaSerializer
+from rest_framework_simplejwt import authentication
+from rest_framework import permissions
 
 class KsiazkaList(APIView):
+
+
+    def get(self,request):
+        ksiazka = Ksiazka.objects.all()
+        serializer = KsiazkaSerializer(ksiazka, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+
+class KsiazkaListProtected(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.JWTAuthentication,)
 
     def get(self,request):
         ksiazka = Ksiazka.objects.all()
--- librarysite/urls.py
+++ librarysite/urls.py
@@ -18,9 +18,17 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from libraryapp import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ksiazka/', views.KsiazkaList.as_view()),
+    path('ksiazkaProtected/', views.KsiazkaListProtected.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
