98ccc924a67141b03a84b0bd456418f482f51b6d
szymongor <gornioczek.szymon@gmail.com>
2017-12-31 14:30:59 +0100

    New ksiazka serializer

--- libraryapp/serializers.py
+++ libraryapp/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
 from .models import Ksiazka, Kategorie
 
-class KsiazkaSerializer(serializers.ModelSerializer):
-    kategoria = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Ksiazka
-        #fields=('syg_ms','tytul')
-        fields='__all__'
 
 class KategorieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Kategorie
-        fields = '__all__'
+        #fields = '__all__'
+        fields= ('id_kategorii', 'kategoria')
+
+class KsiazkaSerializer(serializers.ModelSerializer):
+    #kategoria = serializers.StringRelatedField(many=True)
+    kategoria = KategorieSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ksiazka
+        #fields='__all__'
+        fields=('syg_ms','syg_bg','ozn_opdow','tytul',
+                'tom','rok','isbn_issn','typ','dostepnosc',
+                'kategoria',)
