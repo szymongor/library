a6039620430a8c39720f1ae22e53f5ffb4220088
szymongor <gornioczek.szymon@gmail.com>
2018-01-14 17:22:02 +0100

    Url change

--- librarysite/urls.py
+++ librarysite/urls.py
@@ -31,7 +31,7 @@ from libraryapp.views import CsvImport
 urlpatterns = [
     path('', include('libraryapp.urls')),
     path('admin/', admin.site.urls),
-    path('ksiazka/', views.KsiazkaList.as_view()),
+    path('ksiazka', views.KsiazkaList.as_view()),
     #path('upload/<filename>/', CsvImport.as_view()),
     #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
