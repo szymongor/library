ffa49b08f3a660bc0e8c9666a358bc390fe05638
szymongor <gornioczek.szymon@gmail.com>
2018-03-15 12:28:14 +0100

    Change pass button

--- static/js/add-pass-button.js
+++ static/js/add-pass-button.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
-    setTimeout(function() {
-        addPassButton();
-    }, 100);
+    addPassButton();
 }, false);
 
 function addPassButton() {
     var submit_row = document.getElementsByClassName("submit-row")[0];
     if(submit_row){
-        var button = document.createElement("input");
-        var text = "Nie zapisuj";
-        button.value = text;
-        button.type = "submit";
-        button.name = "_pass";
-        submit_row.appendChild(button);
+        let prevUrl = getPevUrl(window.location.pathname);
+        let passBtn = document.createElement("p");
+        passBtn.classList.add("deletelink-box");
+        style = "background-color: orange;margin-left: 10px;"
+
+        let a = document.createElement("a");
+        a.href=prevUrl;
+        a.classList.add("deletelink");
+        a.setAttribute("style", style);
+        a.innerHTML = "Anuluj zmiany"
+        a.name = "_pass";
+        passBtn.appendChild(a);
+        submit_row.appendChild(passBtn);
     }
 
+}
+
+function getPevUrl(url){
+    let splitedURL = url.split('/');
+    let prevUrl = '/'+splitedURL[1]+'/'+splitedURL[2]
+    return prevUrl;
 }
